d07d1d462399a4624552453f33becb1fa9604e60
Wang Junling <Wang_junling@yeah.net>
2019-12-29 20:23:45 +0800

    Update q1.py
    代码更新

--- q1.py
+++ q1.py
@@ -13,10 +13,31 @@
 
 输入：0219
 输出：3
-
+ABCDEFGHIJKMLNOPQRSTUVWXYZ
 
 '''
 
 def how_many_ways(digitarray):
     # implement here
     
+    s = digitarray.lstrip('0')
+    if not s:
+        return 0
+
+    n = len(s)
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(1, n):
+        #判断10或20的组成可能是BA/K或CA/U,两种变化
+        if s[i] == "0":
+            if (s[i - 1] == "1" or s[i - 1] == "2"):
+                dp[i + 1] = dp[i] + dp[i - 1]
+            else:
+                return 0
+        else:
+            if s[i - 1] == "1" or (s[i - 1] == "2" and 0 <= int(s[i]) <= 5):
+                dp[i + 1] = dp[i] + dp[i - 1]
+            else:
+                dp[i + 1] = dp[i]
+    return dp[-1]
